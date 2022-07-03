bcff5b3cd95ee478d4243ca0cd8efae01d6e73cf
boldyrevda <22929109+BoldyrevDA@users.noreply.github.com>
2022-07-04 00:06:39 +0400

    feat: update layout

--- src/libs/version/AppVersion.vue
+++ src/libs/version/AppVersion.vue
@@ -1,36 +1,6 @@
 <template>
-	<div :class="{ 'right-bottom-corner': bottomRightCorner }" aria-hidden="true">
-		<span class="version"> {{ appVersion }} </span>
-	</div>
+	<span class="version"> {{ appVersion }} </span>
 </template>
-
-<style scoped>
-div {
-	position: fixed;
-	z-index: 100;
-}
-
-.version {
-	animation: smooth 5s ease-in infinite;
-}
-
-@keyframes smooth {
-	0% {
-		opacity: 0.9;
-	}
-	50% {
-		opacity: 0.3;
-	}
-	100% {
-		opacity: 0.9;
-	}
-}
-
-.right-bottom-corner {
-	right: 20px;
-	bottom: 20px;
-}
-</style>
 
 <script lang="ts">
 import Vue from 'vue';
